db4bd39c9ae9dec25f639a25a72e05e25ae7fe33
kcorad4 <59891211+kcorad4@users.noreply.github.com>
2021-09-13 20:05:21 -0400

    Add files via upload

---
+++ src/main/java/Name.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Name {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("what is your name?");
+        String name = sc.next();
+
+        System.out.println("hello, " +name + ", nice to meet you!" );
+    }//end main
+}//end class name
